b5ec59759589928e8c513c96f2d978bc3f1dd8c6
Utkarsh Pandey <utk2665@gmail.com>
2024-10-22 21:06:23 +0530

    Fixed button

--- app/components/VolunteerContent.tsx
+++ app/components/VolunteerContent.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
-import { Link } from 'lucide-react';
+// import { Link } from 'lucide-react';
+import Link from 'next/link';
 import React from 'react';
 
 const VolunteerContent: React.FC = () => {
