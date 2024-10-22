f9a7c6e0ac7caae22c4ea0bd2064ef1735b8086a
Utkarsh Pandey <utk2665@gmail.com>
2024-10-22 21:11:08 +0530

    Showcase build

--- app/volunteer/page.tsx
+++ app/volunteer/page.tsx
@@ -2,15 +2,15 @@ import Link from 'next/link';
 
 const VolunteerSection: React.FC = () => {
   return (
-    <section className="flex flex-col items-center text-center p-6">
+    <section className="flex flex-col items-center justify-center text-center p-6 min-h-screen">
       <h1 className="text-5xl font-bebas mb-4 text-[#94ae81]">
         “Our Kindness, Their Hope!&quot;
       </h1>
-      <p className="font-montserrat text-2xl mb-6 text-gray-900">
+      <p className="font-montserrat text-lg mb-6 text-gray-900">
         Thank you for your interest in volunteering with Care4Poor! We strive to empower underprivileged communities through key initiatives like Score4Change and Debate4Change. Join us to create lasting, meaningful change through education, sports, and public discourse.
       </p>
       <Link href="https://docs.google.com/forms/d/e/1FAIpQLSdx6DjVHiMkUtDRiGDRxRWr9GCAMxMJYoZho7gVyhhKW0pPrA/viewform" passHref className="px-6 py-3 bg-[#94ae81] text-white font-montserrat rounded-3xl hover:opacity-90 transition-opacity">
-          Fill the Form
+        Fill the Form
       </Link>
     </section>
   );
